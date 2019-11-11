41b231f2522c5b4b2a3614a28b6e0c6cf747600b
kafka <kafka0238@gmail.com>
2019-11-11 23:22:43 +0300

    change file script

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function cakes(recipe, available) {
-    var count_cakes = false;
+    count_cakes = false;
+    let cakes = {};
     for (key in recipe) {
         if(!available[key])
             cakes[key] = 0
         else
-            cakes[key] = Math.round(available[key] / recipe[key]);
+            cakes[key] = Math.floor(available[key] / recipe[key]);
     }
     for (key in cakes) {
         if(count_cakes === false || count_cakes > cakes[key]) {
